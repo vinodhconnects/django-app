dd19946eae27ff26d8759112966190b909391544
vinodhconnects <vinodhconnects@gmail.com>
2023-07-21 09:59:16 +0530

    viewedit

--- website/empapp/views.py
+++ website/empapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Employee
 
 # Create your views here.
+
+def app_home(request):
+    employees = Employee.objects.all()  #returns QuerySet
+
+ 
+    context = {
+        'employees': employees,
+        'name':'Employees'
+    }
+    
+    return render(request,'employee.html', context)
