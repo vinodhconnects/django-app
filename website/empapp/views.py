6b2c0039e864fe23f2e47a708ac9f8a71dd57b86
vinodhconnects <vinodhconnects@gmail.com>
2023-07-21 10:33:27 +0530

    urls

--- website/empapp/views.py
+++ website/empapp/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import Employee
-
+from .forms import EmployeeForm
 # Create your views here.
 
 def app_home(request):
     employees = Employee.objects.all()  #returns QuerySet
 
- 
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = EmployeeForm()
     context = {
         'employees': employees,
-        'name':'Employees'
+        'name':'Employees',
+         'form':form
     }
     
     return render(request,'employee.html', context)
