59f42125d62dccea6c3aa21d2589e523a05a33b1
vinodhconnects <vinodhconnects@gmail.com>
2023-07-21 09:31:44 +0530

    model

--- website/empapp/models.py
+++ website/empapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    sno=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30,null=False)
+    city=models.CharField(max_length=30,null=False)
+    
+    def __str__(self):
+        return self.name
