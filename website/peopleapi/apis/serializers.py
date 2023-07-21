670cef17ea20cefee2101b2bec5db8067f6357f8
vinodhconnects <vinodhconnects@gmail.com>
2023-07-21 12:11:31 +0530

    serializer

--- website/peopleapi/apis/serializers.py
+++ website/peopleapi/apis/serializers.py
@@ -2,6 +2,19 @@ from django.db.models import fields
 from rest_framework import serializers
 from peopleapi.models import Product,Supplier
 
+class ProductSerializer(serializers.ModelSerializer):
+    supplier = serializers.CharField(source='supplier.name')
+    class Meta:
+        model= Product
+        fields = "__all__" 
+        #exclude=['productid']   
+    def validate(self,data):
+        if data['price']<0:
+            print('validated ', data['price'])
+            raise serializers.ValidationError('Price is less than zero')
+        else:
+            return data
+
 class SupplierSerializer(serializers.ModelSerializer):
     #products = serializers.StringRelatedField(many=True)
     # products = serializers.HyperlinkedRelatedField(
