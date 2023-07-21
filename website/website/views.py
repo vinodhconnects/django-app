f75054428514062cdc38e161b926c4ff98b3b285
vinodhconnects <vinodhconnects@gmail.com>
2023-07-21 09:17:14 +0530

    viewcommit

--- website/website/views.py
+++ website/website/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # create a function 
 def home_view(request): 
 
-    return render(request,'dhome.html', context={'name':'John'})
+    return render(request,'whome.html', context={'appname':'ABC inc'})
