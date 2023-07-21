8c59070cec7fc7cc5b42132bc3c926a61b558369
vinodhconnects <vinodhconnects@gmail.com>
2023-07-21 12:16:02 +0530

    papi

--- website/peopleapi/apis/views.py
+++ website/peopleapi/apis/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from peopleapi.models import Product,Supplier
 from rest_framework.decorators import api_view,APIView
 from rest_framework.response import Response
-from peopleapi.apis.serializers import SupplierSerializer
+from peopleapi.apis.serializers import ProductSerializer,SupplierSerializer
 from rest_framework import status
+class  ProductsAPI(APIView):
+
+    def get(self,request,pk=None):
+        if(pk==None):
+            productlist=Product.objects.all()
+            result=ProductSerializer(productlist,many=True,context={'request':request})
+            return Response(result.data)
+        else:
+            try:
+                item=Product.objects.get(pk=pk)
+                return Response(ProductSerializer(item).data)
+            except Product.DoesNotExist:
+                return Response({'error':'No Content'}, status=status.HTTP_204_NO_CONTENT)
 
 class  SuppliersAPI(APIView):
 
